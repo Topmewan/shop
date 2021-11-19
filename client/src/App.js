c830547c8f40f28403e01ab77abc5322bbfc9bd5
Topmewan <anon8733@gmail.com>
2021-11-19 18:04:02 +0300

    initial

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+
+import product from './data.json';
+
+import styles from './App.module.css';
+
+
+function App() {
+
+
+  return (
+          <div className='grid-container'>
+              fdfg
+
+
+          </div>
+  );
+}
+
+export default App;
+
+
---
+++ client/src/pages/HomePage/Home.jsx
@@ -0,0 +1,10 @@
+import styles from './Home.module.css';
+const Home = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Home;
