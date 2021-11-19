5d9f665500adddcccaf18a758d1f1ac7ca87eeba
Topmewan <anon8733@gmail.com>
2021-11-19 19:02:43 +0300

    navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import cart from './img/cart.svg';
+import {Link} from "react-router-dom";
+
+import styles from './Navbar.module.css';
 
 const Navbar = () => {
     return (
-        <div>
+        <nav className={styles.nav__bar}>
 
-        </div>
+            <Link to='/'>
+                <h2>Магазин</h2>
+            </Link>
+
+            <Link to='/cart'>
+                <div className={styles.nav__cart}>
+
+                    <img className={styles.cart__img} src={cart} alt="cart"/>
+                    <span className={styles.cart__quantity}>
+                        <span>0</span>
+                    </span>
+
+                </div>
+
+            </Link>
+
+        </nav>
     );
 };
 
