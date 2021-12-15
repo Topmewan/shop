d61fce3a41efa8b2e3c0406b44cf594bda023d77
Topmewan <anon8733@gmail.com>
2021-12-15 15:29:22 +0300

    fix

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -19,11 +19,9 @@ const Navbar = () => {
 
 	return (
 		<nav className={styles.nav__bar}>
-
 			<Link to="/">
 				<h2>Магазин</h2>
 			</Link>
-
 			<Link to="/cart">
 				<div className={styles.nav__cart}>
 					<img className={styles.cart__img} src={cartIcon} alt="cart" />
